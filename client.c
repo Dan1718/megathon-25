c4ab1bd8b1653165a2b472a8f53d9f49305ebbb0
Dan1718 <danielvincent1718@gmail.com>
2025-10-11 21:49:26 +0530

    client.c ip addr change

--- client.c
+++ client.c
@@ -1,111 +1,61 @@
-#include <libwebsockets.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
-static struct lws *web_socket = NULL;
+#define PORT 9001
 
-#define EXAMPLE_TX_BUFFER_BYTES 10
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    const char *hello = "Hello from client";
+    char buffer[1024] = {0};
 
-static int callback_example( struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len )
-{
-	switch( reason )
-	{
-		case LWS_CALLBACK_CLIENT_ESTABLISHED:
-			lws_callback_on_writable( wsi );
-			break;
+    // 1. Creating socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
 
-		case LWS_CALLBACK_CLIENT_RECEIVE:
-			/* Handle incomming messages here. */
-			break;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
 
-		case LWS_CALLBACK_CLIENT_WRITEABLE:
-		{
-			unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + EXAMPLE_TX_BUFFER_BYTES + LWS_SEND_BUFFER_POST_PADDING];
-			unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
-			size_t n = sprintf( (char *)p, "%u", rand() );
-			lws_write( wsi, p, n, LWS_WRITE_TEXT );
-			break;
-		}
+    // 2. Connecting to the server
+    // Convert IPv4 addresses from text to binary form
+    // "127.0.0.1" is the loopback address, meaning "this same machine".
+    // If your server is on another machine, you would put its IP address here.
+    if (inet_pton(AF_INET, "10.53.138.156", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
 
-		case LWS_CALLBACK_CLIENT_CLOSED:
-		case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:
-			web_socket = NULL;
-			break;
+    // Connect to the server socket
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
 
-		default:
-			break;
-	}
+    printf("Connected to server.\n");
+    char string[100]; 
+    scanf(" %s",string);
+    
+    send(sock, string, strlen(string), 0);
 
-	return 0;
-}
 
-enum protocols
-{
-	PROTOCOL_EXAMPLE = 0,
-	PROTOCOL_COUNT
-};
+    // 4. Reading the server's response
+    read(sock, buffer, 1024);
+    printf("Server response: %s\n", buffer);
+    scanf("%s",string);
+    send(sock, string, strlen(string), 0);
+    
 
-static struct lws_protocols protocols[] =
-{
-    {
-        .name                  = "example-protocol", /* Protocol name*/
-        .callback              = callback_example,   /* Protocol callback */
-        .per_session_data_size = 0,                  /* Protocol callback 'userdata' size */
-        .rx_buffer_size        = 0,                  /* Receve buffer size (0 = no restriction) */
-        .id                    = 0,                  /* Protocol Id (version) (optional) */
-        .user                  = NULL,               /* 'User data' ptr, to access in 'protocol callback */
-        .tx_packet_size        = 0                   /* Transmission buffer size restriction (0 = no restriction) */
-    },
-    LWS_PROTOCOL_LIST_TERM /* terminator */
-};
+    send(sock, string, strlen(string), 0);
 
-int main( int argc, char *argv[] )
-{
-	struct lws_context_creation_info info;
-	memset( &info, 0, sizeof(info) );
 
-	info.port = CONTEXT_PORT_NO_LISTEN; /* we do not run any server */
-	info.protocols = protocols;
-	info.gid = -1;
-	info.uid = -1;
+    // 5. Closing the socket
+    close(sock);
 
-	struct lws_context *context = lws_create_context( &info );
-
-	time_t old = 0;
-	while( 1 )
-	{
-		struct timeval tv;
-		gettimeofday( &tv, NULL );
-
-		/* Connect if we are not connected to the server. */
-		if( !web_socket && tv.tv_sec != old )
-		{
-			struct lws_client_connect_info ccinfo;
-			memset(&ccinfo, 0, sizeof(ccinfo));
-			
-			ccinfo.context = context;
-			ccinfo.address = "localhost";
-			ccinfo.port = 8000;
-			ccinfo.path = "/";
-			ccinfo.host = lws_canonical_hostname( context );
-			ccinfo.origin = "origin";
-			ccinfo.protocol = protocols[PROTOCOL_EXAMPLE].name;
-			
-			web_socket = lws_client_connect_via_info(&ccinfo);
-		}
-
-		if( tv.tv_sec != old )
-		{
-			/* Send a random number to the server every second. */
-			lws_callback_on_writable( web_socket );
-			old = tv.tv_sec;
-		}
-
-		lws_service( context, /* timeout_ms = */ 250 ); /* NOTE: since v3.2, timeout_ms may be set to '0', since it internally ignored */
-	}
-
-	lws_context_destroy( context );
-
-	return 0;
+    return 0;
 }
